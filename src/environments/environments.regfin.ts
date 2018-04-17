ec761b97dc32d5d16331e0fdece765604d7726aa
Rahul Bairathi <rbairath@uncc.edu>
2018-04-17 13:09:34 -0400

    Development

---
+++ src/environments/environments.regfin.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serviceURL: 'http://localhost:9000/',
+  updateURL: 'http://localhost:5100/bcsgw/rest/v1/transaction/invocation',
+  queryURL: 'http://localhost:5100/bcsgw/rest/v1/transaction/query',
+  persona: 'FinCEN'
+};
